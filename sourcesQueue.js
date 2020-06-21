116709f8d4655388e33b34d2b58370b8ba072156
Jorres Tarasov <jorres.tarasov@gmail.com>
2020-06-22 01:03:02 +0300

    check if present first

--- sourcesQueue.js
+++ sourcesQueue.js
@@ -13,7 +13,7 @@ var sourceToNames = {};
 var nameToDates = {};
 var sourcesQueue = {
     selectSourceToRun: function (creep) {
-        var e_1, _a, e_2, _b;
+        var e_1, _a, e_2, _b, e_3, _c;
         var sources = creep.room.find(FIND_SOURCES);
         try {
             for (var sources_1 = __values(sources), sources_1_1 = sources_1.next(); !sources_1_1.done; sources_1_1 = sources_1.next()) {
@@ -31,19 +31,12 @@ var sourcesQueue = {
             }
             finally { if (e_1) throw e_1.error; }
         }
-        var bestCoef = -1;
-        var bestSource;
         try {
             for (var sources_2 = __values(sources), sources_2_1 = sources_2.next(); !sources_2_1.done; sources_2_1 = sources_2.next()) {
                 var source = sources_2_1.value;
                 if (sourceToNames[source.toString()].has(creep.name)) {
                     console.log("creep " + creep.name + " already to source " + source.toString());
                     return source;
-                }
-                var curCoef = checkIfGoTo(creep, source);
-                if (curCoef > bestCoef) {
-                    bestSource = source;
-                    bestCoef = curCoef;
                 }
             }
         }
@@ -54,30 +47,49 @@ var sourcesQueue = {
             }
             finally { if (e_2) throw e_2.error; }
         }
-        return bestSource;
-    },
-    cleanIntentionForSource: function (creep) {
-        var e_3, _a;
-        var sources = creep.room.find(FIND_SOURCES);
+        var bestCoef = -1;
+        var bestSource;
         try {
             for (var sources_3 = __values(sources), sources_3_1 = sources_3.next(); !sources_3_1.done; sources_3_1 = sources_3.next()) {
                 var source = sources_3_1.value;
-                if (sourceToNames[source.toString()].has(creep.name)) {
-                    sourceToNames[source.toString()]["delete"](creep.name);
+                var curCoef = checkIfGoTo(creep, source);
+                if (curCoef > bestCoef) {
+                    bestSource = source;
+                    bestCoef = curCoef;
                 }
             }
         }
         catch (e_3_1) { e_3 = { error: e_3_1 }; }
         finally {
             try {
-                if (sources_3_1 && !sources_3_1.done && (_a = sources_3["return"])) _a.call(sources_3);
+                if (sources_3_1 && !sources_3_1.done && (_c = sources_3["return"])) _c.call(sources_3);
             }
             finally { if (e_3) throw e_3.error; }
+        }
+        return bestSource;
+    },
+    cleanIntentionForSource: function (creep) {
+        var e_4, _a;
+        var sources = creep.room.find(FIND_SOURCES);
+        try {
+            for (var sources_4 = __values(sources), sources_4_1 = sources_4.next(); !sources_4_1.done; sources_4_1 = sources_4.next()) {
+                var source = sources_4_1.value;
+                if (sourceToNames[source.toString()].has(creep.name)) {
+                    sourceToNames[source.toString()]["delete"](creep.name);
+                }
+            }
+        }
+        catch (e_4_1) { e_4 = { error: e_4_1 }; }
+        finally {
+            try {
+                if (sources_4_1 && !sources_4_1.done && (_a = sources_4["return"])) _a.call(sources_4);
+            }
+            finally { if (e_4) throw e_4.error; }
         }
     }
 };
 function checkIfGoTo(creep, source) {
-    var e_4, _a;
+    var e_5, _a;
     var myBorders = determineMyLimits(creep, source);
     var ans = 0;
     try {
@@ -103,12 +115,12 @@ function checkIfGoTo(creep, source) {
             }
         }
     }
-    catch (e_4_1) { e_4 = { error: e_4_1 }; }
+    catch (e_5_1) { e_5 = { error: e_5_1 }; }
     finally {
         try {
             if (_c && !_c.done && (_a = _b["return"])) _a.call(_b);
         }
-        finally { if (e_4) throw e_4.error; }
+        finally { if (e_5) throw e_5.error; }
     }
     return 1 / ans;
 }
@@ -127,8 +139,6 @@ function parseCreepBody(creep) {
         'CARRY': 1,
         'MOVE': 2
     };
-    console.log('body parts are :');
-    console.log(ans);
     return ans;
 }
 module.exports = sourcesQueue;
