0a173d6255844117f2972a89f263d2edf94983a7
Jorres Tarasov <jorres.tarasov@gmail.com>
2020-06-23 14:42:38 +0300

    simple logic based on freeTilesNear

--- sourcesQueue.js
+++ sourcesQueue.js
@@ -11,18 +11,25 @@ var __values = (this && this.__values) || function(o) {
 };
 var config = require('config');
 var terrain = config.terrain();
-var sourceToNames = {};
-var nameToDates = {};
+var sourcesToNames = new Map();
+var freePlacesAtSource = new Map();
 var sourcesQueue = {
     selectSourceToRun: function (creep) {
-        var e_1, _a, e_2, _b, e_3, _c;
+        var e_1, _a;
         var sources = creep.room.find(FIND_SOURCES);
+        initNewSources(sources);
+        var bestSourceId;
+        var bestSourceFreePlaces = -1;
         try {
             for (var sources_1 = __values(sources), sources_1_1 = sources_1.next(); !sources_1_1.done; sources_1_1 = sources_1.next()) {
                 var source = sources_1_1.value;
-                if (sourceToNames[source.toString()] === undefined) {
-                    console.log("creating new Set for source " + source.toString());
-                    sourceToNames[source.toString()] = new Set();
+                if (sourcesToNames.get(source.id).has(creep.name)) {
+                    return U.getById(source.id);
+                }
+                var curFreePlaces = freePlacesAtSource.get(source.id);
+                if (curFreePlaces > bestSourceFreePlaces) {
+                    bestSourceFreePlaces = curFreePlaces;
+                    bestSourceId = source.id;
                 }
             }
         }
@@ -33,111 +40,29 @@ var sourcesQueue = {
             }
             finally { if (e_1) throw e_1.error; }
         }
+        return U.getById(bestSourceId);
+    },
+    cleanIntentionForSource: function (creep) {
+        var e_2, _a;
+        var sources = creep.room.find(FIND_SOURCES);
+        initNewSources(sources);
         try {
             for (var sources_2 = __values(sources), sources_2_1 = sources_2.next(); !sources_2_1.done; sources_2_1 = sources_2.next()) {
                 var source = sources_2_1.value;
-                if (sourceToNames[source.toString()].has(creep.name)) {
-                    console.log("creep " + creep.name + " already to source " + source.toString());
-                    return source;
+                if (sourcesToNames.get(source.id).has(creep.name)) {
+                    freePlacesAtSource.set(source.id, freePlacesAtSource.get(source.id) + 1);
                 }
             }
         }
         catch (e_2_1) { e_2 = { error: e_2_1 }; }
         finally {
             try {
-                if (sources_2_1 && !sources_2_1.done && (_b = sources_2["return"])) _b.call(sources_2);
+                if (sources_2_1 && !sources_2_1.done && (_a = sources_2["return"])) _a.call(sources_2);
             }
             finally { if (e_2) throw e_2.error; }
         }
-        var bestCoef = -1;
-        var bestSource;
-        var bestBorders = { first: -1, second: -1 };
-        try {
-            for (var sources_3 = __values(sources), sources_3_1 = sources_3.next(); !sources_3_1.done; sources_3_1 = sources_3.next()) {
-                var source = sources_3_1.value;
-                var myBorders = determineMyLimits(creep, source);
-                var curCoef = checkIfGoTo(creep, source, myBorders);
-                if (curCoef > bestCoef) {
-                    bestSource = source;
-                    bestBorders = myBorders;
-                    bestCoef = curCoef;
-                }
-            }
-        }
-        catch (e_3_1) { e_3 = { error: e_3_1 }; }
-        finally {
-            try {
-                if (sources_3_1 && !sources_3_1.done && (_c = sources_3["return"])) _c.call(sources_3);
-            }
-            finally { if (e_3) throw e_3.error; }
-        }
-        if (bestBorders.first == -1) {
-            console.log("alarm, bestBorders not set");
-            return sources[0];
-        }
-        else {
-            sourceToNames[bestSource.toString()].add(creep.name);
-            nameToDates[creep.name] = bestBorders;
-            return bestSource;
-        }
-    },
-    cleanIntentionForSource: function (creep) {
-        var e_4, _a;
-        var sources = creep.room.find(FIND_SOURCES);
-        try {
-            for (var sources_4 = __values(sources), sources_4_1 = sources_4.next(); !sources_4_1.done; sources_4_1 = sources_4.next()) {
-                var source = sources_4_1.value;
-                if (sourceToNames[source.toString()].has(creep.name)) {
-                    sourceToNames[source.toString()]["delete"](creep.name);
-                }
-            }
-        }
-        catch (e_4_1) { e_4 = { error: e_4_1 }; }
-        finally {
-            try {
-                if (sources_4_1 && !sources_4_1.done && (_a = sources_4["return"])) _a.call(sources_4);
-            }
-            finally { if (e_4) throw e_4.error; }
-        }
     }
 };
-function checkIfGoTo(creep, source, myBorders) {
-    var e_5, _a;
-    var ans = 1;
-    try {
-        for (var _b = __values(sourceToNames[source.toString()]), _c = _b.next(); !_c.done; _c = _b.next()) {
-            var competitor = _c.value;
-            var anotherBorders = nameToDates[competitor];
-            var borders = [myBorders.first, myBorders.second];
-            borders.push(anotherBorders.first);
-            borders.push(anotherBorders.second);
-            borders.sort();
-            var start = false;
-            var end = false;
-            for (var i = 0; i < 4; i++) {
-                if (borders[i] == myBorders.first) {
-                    start = true;
-                }
-                if (borders[i] == anotherBorders.second) {
-                    end = true;
-                }
-                if (start && !end) {
-                    ans += borders[i] - borders[i - 1];
-                }
-            }
-        }
-    }
-    catch (e_5_1) { e_5 = { error: e_5_1 }; }
-    finally {
-        try {
-            if (_c && !_c.done && (_a = _b["return"])) _a.call(_b);
-        }
-        finally { if (e_5) throw e_5.error; }
-    }
-    var result = (1 / ans) * freeTilesNear(source.pos);
-    console.log(result);
-    return result;
-}
 function freeTilesNear(pos) {
     var x = pos.x;
     var y = pos.y;
@@ -151,21 +76,23 @@ function freeTilesNear(pos) {
     }
     return ans;
 }
-function determineMyLimits(creep, source) {
-    var pathToSource = creep.room.findPath(creep.pos, source.pos);
-    var creepBody = parseCreepBody(creep);
-    var step = creepBody['ALL'] / creepBody['WORK'];
-    var left = Math.floor(pathToSource.length * step);
-    var right = Math.floor(left + (creepBody['CARRY'] * 50 / creepBody['WORK']));
-    return { first: left + Game.time, second: right + Game.time };
-}
-function parseCreepBody(creep) {
-    var ans = {
-        'ALL': 4,
-        'WORK': 1,
-        'CARRY': 1,
-        'MOVE': 2
-    };
-    return ans;
+function initNewSources(sources) {
+    var e_3, _a;
+    try {
+        for (var sources_3 = __values(sources), sources_3_1 = sources_3.next(); !sources_3_1.done; sources_3_1 = sources_3.next()) {
+            var source = sources_3_1.value;
+            if (!sourcesToNames.get(source.id)) {
+                sourcesToNames.set(source.id, new Set());
+                freePlacesAtSource.set(source.id, freeTilesNear(source.pos));
+            }
+        }
+    }
+    catch (e_3_1) { e_3 = { error: e_3_1 }; }
+    finally {
+        try {
+            if (sources_3_1 && !sources_3_1.done && (_a = sources_3["return"])) _a.call(sources_3);
+        }
+        finally { if (e_3) throw e_3.error; }
+    }
 }
 module.exports = sourcesQueue;
--- ts_src/sourcesQueue.ts
+++ ts_src/sourcesQueue.ts
@@ -2,88 +2,40 @@
 var config = require('config');
 var terrain = config.terrain();
 
-var sourceToNames: StringHashMap<Set<string>> = {};
-var nameToDates: StringHashMap<Pair<number, number>> = {};
+var sourcesToNames: Map<string, Set<string>> = new Map();
+var freePlacesAtSource: Map<string, number> = new Map();
 
 var sourcesQueue = {
     selectSourceToRun: function(creep: Creep): Source {
         let sources = creep.room.find(FIND_SOURCES);
+        initNewSources(sources);
+        let bestSourceId;
+        let bestSourceFreePlaces = -1;
         for (let source of sources) {
-            if (sourceToNames[source.toString()] === undefined) {
-                console.log("creating new Set for source " + source.toString());
-                sourceToNames[source.toString()] = new Set();
+            if (sourcesToNames.get(source.id).has(creep.name)) {
+                return U.getById(source.id);
+            }
+
+            let curFreePlaces = freePlacesAtSource.get(source.id);
+            if (curFreePlaces > bestSourceFreePlaces) {
+                bestSourceFreePlaces = curFreePlaces;
+                bestSourceId = source.id;
             }
         }
 
-        for (let source of sources) {
-            if (sourceToNames[source.toString()].has(creep.name)) {
-                console.log("creep " + creep.name + " already to source " + source.toString());
-                return source;
-            }
-        }
-
-        let bestCoef: number = -1;
-        let bestSource: Source;
-        let bestBorders: Pair<number, number> = {first: -1, second: -1};
-
-        for (let source of sources) {
-            let myBorders: Pair<number, number> = determineMyLimits(creep, source);
-            let curCoef = checkIfGoTo(creep, source, myBorders);
-            if (curCoef > bestCoef) {
-                bestSource = source;
-                bestBorders = myBorders;
-                bestCoef = curCoef;
-            }
-        }
-
-        if (bestBorders.first == -1) {
-            console.log("alarm, bestBorders not set");
-            return sources[0];
-        } else {
-            sourceToNames[bestSource.toString()].add(creep.name);
-            nameToDates[creep.name] = bestBorders;
-            return bestSource;
-        }
+        return U.getById(bestSourceId);
     },
 
     cleanIntentionForSource: function(creep: Creep): void {
         let sources = creep.room.find(FIND_SOURCES);
+        initNewSources(sources);
         for (let source of sources) {
-            if (sourceToNames[source.toString()].has(creep.name)) {
-                sourceToNames[source.toString()].delete(creep.name);
+            if (sourcesToNames.get(source.id).has(creep.name)) {
+                freePlacesAtSource.set(source.id, freePlacesAtSource.get(source.id) + 1);
             }
         }
     }
 };
-
-function checkIfGoTo(creep: Creep, source: Source, myBorders: Pair<number, number>): number {
-    let ans: number = 1;
-    for (let competitor of sourceToNames[source.toString()]) {
-        let anotherBorders: Pair<number, number> = nameToDates[competitor];
-        let borders: number[] = [myBorders.first, myBorders.second];
-        borders.push(anotherBorders.first);
-        borders.push(anotherBorders.second);
-
-        borders.sort();
-
-        let start: boolean = false;
-        let end: boolean = false;
-        for (let i = 0; i < 4; i++) {
-            if (borders[i] == myBorders.first) {
-                start = true;
-            }
-            if (borders[i] == anotherBorders.second) {
-                end = true;
-            }
-            if (start && !end) {
-                ans += borders[i] - borders[i - 1];
-            }
-        }
-    }
-    let result = (1 / ans) * freeTilesNear(source.pos);
-    console.log(result);
-    return result;
-}
 
 function freeTilesNear(pos: RoomPosition): number {
     let x = pos.x;
@@ -101,34 +53,13 @@ function freeTilesNear(pos: RoomPosition): number {
     return ans;
 }
 
-function determineMyLimits(creep: Creep, source: Source): Pair<number, number> {
-    const pathToSource = creep.room.findPath(creep.pos, source.pos);
-    const creepBody: StringHashMap<number> = parseCreepBody(creep);
-    const step: number = creepBody['ALL'] / creepBody['WORK'];
-    const left: number = Math.floor(pathToSource.length * step);
-    const right: number = Math.floor(left + (creepBody['CARRY'] * 50 / creepBody['WORK']))
-    return {first: left + Game.time, second: right + Game.time};
-}
-
-function parseCreepBody(creep: Creep): StringHashMap<number> {
-    // let ans: StringHashMap<number> = {};
-    // let bodyparts = creep.body;
-    // ans['ALL'] = 0;
-    // for (let bodypart of bodyparts) {
-    //     let bodypartName: string = bodypart.type.toString();
-    //     if (ans.hasOwnProperty(bodypartName)) {
-    //         ans[bodypartName] = 0;
-    //     }
-    //     ans[bodypartName]++;
-    //     ans['ALL']++;
-    // }
-    let ans: StringHashMap<number> = {
-        'ALL': 4,
-        'WORK': 1,
-        'CARRY': 1,
-        'MOVE': 2
-    };
-    return ans;
+function initNewSources(sources: Source[]): void {
+    for (let source of sources) {
+        if (!sourcesToNames.get(source.id)) {
+            sourcesToNames.set(source.id, new Set());
+            freePlacesAtSource.set(source.id, freeTilesNear(source.pos));
+        }
+    }
 }
 
 // @ts-ignore
